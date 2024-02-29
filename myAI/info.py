c465abd8f5a63582fda97079dbdc677938b8e4a2
Arun Rathore <15arunrathore@gmail.com>
2024-02-29 13:52:45 +0530

    fourth_commit

--- myAI/info.py
+++ myAI/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'arunrathore@myyahoo.com'
-EMAIL_HOST_PASSWORD = 'Yahoomail08'
+EMAIL_HOST_USER = 'Ebter your email'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
